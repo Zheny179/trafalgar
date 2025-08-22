772125343bc9dca9d2fd3c7b59a842621ce692ef
Evgeny <doronine222@gmail.com>
2025-08-23 03:58:25 +0700

    init project

---
+++ prettier.config.js
@@ -0,0 +1,23 @@
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: "es5",
+  arrowParens: "always",
+  bracketSpacing: true,
+  quoteProps: "consistent",
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: false,
+  endOfLine: "lf",
+  singleAttributePerLine: true,
+  bracketSameLine: false,
+  embeddedLanguageFormatting: "auto",
+  insertPragma: false,
+  requirePragma: false,
+}
+
+
